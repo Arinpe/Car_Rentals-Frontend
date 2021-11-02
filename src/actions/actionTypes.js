ef9bf92380634d0f25fc9e408fd81a5d64f44356
Arinpe <dehtolah@gmail.com>
2021-11-02 10:01:47 +0100

    Add the src folder

--- src/actions/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const SET_USER = 'SET_USER';
-export const SET_HOUSES = 'SET_HOUSES';
-export const SET_HOUSE = 'SET_HOUSE';
-export const SET_FAVOURITE = 'SET_FAVOURITE';
-export const SET_LOADING = 'SET_LOADING';
--- src/actions/loadingAction.js
+++
@@ -1,3 +0,0 @@
-import * as types from './actionTypes';
-
-export default status => ({ type: types.SET_LOADING, status });
